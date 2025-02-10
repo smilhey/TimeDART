db700b0e46b476bcf102dcd60f49f9b88c4f4602
unknown <matthieu.souda@laposte.net>
2025-02-10 15:21:25 +0100

    Adding TCN

---
+++ models/TCN.py
@@ -0,0 +1,261 @@
+import torch
+import torch.nn as nn
+from .layers.tcn import TCNEncoder, TCNDecoder
+from .layers.encoder_decoder import (
+    CausalTransformerEncoder,
+    MaskedDenoisingPatchDecoder,
+)
+from .layers.diffusion import Diffusion
+from .layers.preprocess import (
+    ChannelIndependence,
+    Patch,
+    PatchEmbedding,
+    PosEncoding,
+    AddSosTokenAndDropLast,
+)
+
+
+class FlattenHead(nn.Module):
+    def __init__(
+        self,
+        seq_len: int,
+        d_model: int,
+        pred_len: int,
+        dropout: float,
+    ):
+        super(FlattenHead, self).__init__()
+        self.pred_len = pred_len
+        self.flatten = nn.Flatten(start_dim=-2)
+        self.forecast_head = nn.Linear(seq_len * d_model, pred_len)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        :param x: [batch_size, num_features, seq_len, d_model]
+        :return: [batch_size, pred_len, num_features]
+        """
+        x = self.flatten(x)  # (batch_size, num_features, seq_len * d_model)
+        x = self.forecast_head(x)  # (batch_size, num_features, pred_len)
+        x = self.dropout(x)  # (batch_size, num_features, pred_len)
+        x = x.permute(0, 2, 1)  # (batch_size, pred_len, num_features)
+        return x
+
+
+class Model(nn.Module):
+    """
+    TimeDART
+    """
+
+    def __init__(self, args):
+        super(Model, self).__init__()
+        self.input_len = args.input_len
+
+        # For Model Hyperparameters
+        self.d_model = args.d_model
+        self.num_heads = args.n_heads
+        self.feedforward_dim = args.d_ff
+        self.dropout = args.dropout
+        self.device = args.device
+        self.task_name = args.task_name
+        self.pred_len = args.pred_len
+        self.use_norm = args.use_norm
+        self.channel_independence = ChannelIndependence(
+            input_len=self.input_len,
+        )
+
+        # Patch
+        self.patch_len = args.patch_len
+        self.stride = self.patch_len  # non-overlapping
+        self.patch = Patch(
+            patch_len=self.patch_len,
+            stride=self.stride,
+        )
+        self.seq_len = int((self.input_len - self.patch_len) / self.stride) + 1
+
+        # Embedding
+        self.enc_embedding = PatchEmbedding(
+            patch_len=self.patch_len,
+            d_model=self.d_model,
+        )
+
+        self.positional_encoding = PosEncoding(
+            d_model=self.d_model,
+            dropout=self.dropout,
+            device=self.device,
+        )
+
+        sos_token = torch.randn(1, 1, self.d_model, device=self.device)
+        self.sos_token = nn.Parameter(sos_token, requires_grad=True)
+
+        self.add_sos_token_and_drop_last = AddSosTokenAndDropLast(
+            sos_token=self.sos_token,
+        )
+
+        # Encoder (Casual Trasnformer)
+        self.diffusion = Diffusion(
+            time_steps=args.time_steps,
+            device=self.device,
+            scheduler=args.scheduler,
+        )
+
+        self.encoder = TCNEncoder(
+            input_size=args.d_model,
+            d_model=args.d_model,
+            num_layers=args.e_layers,
+            kernel_size=3,
+            dropout=args.dropout,
+        )
+        # self.encoder = CausalTransformerEncoder(
+        #     d_model=args.d_model,
+        #     num_heads=args.n_heads,
+        #     feedforward_dim=args.d_ff,
+        #     dropout=args.dropout,
+        #     num_layers=args.e_layers,
+        # )
+
+        # Decoder
+        if self.task_name == "pretrain":
+            self.tcn_decoder = TCNDecoder(
+                d_model=args.d_model,
+                num_layers=args.d_layers,  # Adjust this based on your needs
+                kernel_size=3,
+                dropout=args.dropout
+            )
+            # self.denoising_patch_decoder = MaskedDenoisingPatchDecoder(
+            #     d_model=args.d_model,
+            #     num_layers=args.d_layers,
+            #     num_heads=args.n_heads,
+            #     feedforward_dim=args.d_ff,
+            #     dropout=args.dropout,
+            #     mask_ratio=args.mask_ratio,
+            # )
+
+            self.projection = FlattenHead(
+                seq_len=self.seq_len,
+                d_model=self.d_model,
+                pred_len=args.input_len,
+                dropout=args.head_dropout,
+            )
+            # self.projection = ARFlattenHead(
+            #     d_model=self.d_model,
+            #     patch_len=self.patch_len,
+            #     dropout=args.head_dropout,
+            # )
+
+        elif self.task_name == "finetune":
+            self.head = FlattenHead(
+                seq_len=self.seq_len,
+                d_model=args.d_model,
+                pred_len=args.pred_len,
+                dropout=args.head_dropout,
+            )
+
+    def pretrain(self, x):
+        # [batch_size, input_len, num_features]
+        batch_size, input_len, num_features = x.size()
+        if self.use_norm:
+            # Instance Normalization
+            means = torch.mean(
+                x, dim=1, keepdim=True
+            ).detach()  # [batch_size, 1, num_features], detach from gradient
+            x = x - means  # [batch_size, input_len, num_features]
+            stdevs = torch.sqrt(
+                torch.var(x, dim=1, keepdim=True, unbiased=False) + 1e-5
+            ).detach()  # [batch_size, 1, num_features]
+            x = x / stdevs  # [batch_size, input_len, num_features]
+
+        # Channel Independence
+        x = self.channel_independence(x)  # [batch_size * num_features, input_len, 1]
+        # Patch
+        x_patch = self.patch(x)  # [batch_size * num_features, seq_len, patch_len]
+
+        # For Casual Transformer
+        x_embedding = self.enc_embedding(
+            x_patch
+        )  # [batch_size * num_features, seq_len, d_model]
+        x_embedding_bias = self.add_sos_token_and_drop_last(
+            x_embedding
+        )  # [batch_size * num_features, seq_len, d_model]
+        x_embedding_bias = self.positional_encoding(x_embedding_bias)
+        x_out = self.encoder(
+            x_embedding_bias,
+            is_mask=True,
+        )  # [batch_size * num_features, seq_len, d_model]
+
+        # Diffusion
+        noise_x_patch, noise, t = self.diffusion(
+            x_patch
+        )  # [batch_size * num_features, seq_len, patch_len]
+        noise_x_embedding = self.enc_embedding(
+            noise_x_patch
+        )  # [batch_size * num_features, seq_len, d_model]
+        noise_x_embedding = self.positional_encoding(noise_x_embedding)
+
+        # For Denoising Patch Decoder
+        # predict_x = self.tcn_decoder(x_out)  # [batch_size, seq_len, d_model]
+        predict_x = self.denoising_patch_decoder(
+            query=noise_x_embedding,
+            key=x_out,
+            value=x_out,
+            is_tgt_mask=True,
+            is_src_mask=True,
+        )  # [batch_size * num_features, seq_len, d_model]
+
+        # For Decoder
+        predict_x = predict_x.reshape(
+            batch_size, num_features, -1, self.d_model
+        )  # [batch_size, num_features, seq_len, d_model]
+        predict_x = self.projection(predict_x)  # [batch_size, input_len, num_features]
+
+        # Instance Denormalization
+        if self.use_norm:
+            predict_x = predict_x * (stdevs[:, 0, :].unsqueeze(1)).repeat(
+                1, input_len, 1
+            )  # [batch_size, input_len, num_features]
+            predict_x = predict_x + (means[:, 0, :].unsqueeze(1)).repeat(
+                1, input_len, 1
+            )  # [batch_size, input_len, num_features]
+
+        return predict_x
+
+    def forecast(self, x):
+        batch_size, _, num_features = x.size()
+        if self.use_norm:
+            means = torch.mean(x, dim=1, keepdim=True).detach()
+            x = x - means
+            stdevs = torch.sqrt(
+                torch.var(x, dim=1, keepdim=True, unbiased=False) + 1e-5
+            ).detach()
+            x = x / stdevs
+
+        x = self.channel_independence(x)  # [batch_size * num_features, input_len, 1]
+        x = self.patch(x)  # [batch_size * num_features, seq_len, patch_len]
+        x = self.enc_embedding(x)  # [batch_size * num_features, seq_len, d_model]
+        x = self.positional_encoding(x)  # [batch_size * num_features, seq_len, d_model]
+        x = self.encoder(
+            x,
+            is_mask=False,
+        )  # [batch_size * num_features, seq_len, d_model]
+        x = x.reshape(
+            batch_size, num_features, -1, self.d_model
+        )  # [batch_size, num_features, seq_len, d_model]
+
+        # forecast
+        x = self.head(x)  # [bs, pred_len, n_vars]
+
+        # denormalization
+        if self.use_norm:
+            x = x * (stdevs[:, 0, :].unsqueeze(1)).repeat(1, self.pred_len, 1)
+            x = x + (means[:, 0, :].unsqueeze(1)).repeat(1, self.pred_len, 1)
+
+        return x
+
+    def forward(self, batch_x):
+
+        if self.task_name == "pretrain":
+            return self.pretrain(batch_x)
+        elif self.task_name == "finetune":
+            dec_out = self.forecast(batch_x)
+            return dec_out[:, -self.pred_len :, :]
+        else:
+            raise ValueError("task_name should be 'pretrain' or 'finetune'")
